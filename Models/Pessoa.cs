9fb9a6bb071e7f1e7b17fc7f7611e62a4b3aa36d
Juann-SPS <53199604+Juann-SPS@users.noreply.github.com>
2020-06-23 01:01:29 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciamentoPessoa.Models {
+    public class Pessoa {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
